d1a68c1f93f01982e1fa6e31b47a899349bfc73f
AmruthSD <amruth1908@gmail.com>
2025-03-11 17:11:21 +0530

    Added functionality to respond to multiple pings

--- cmd/Redis/main.go
+++ cmd/Redis/main.go
@@ -6,6 +6,30 @@ import (
 	"os"
 )
 
+func handleConnection(con net.Conn) {
+	defer con.Close()
+
+	buf := make([]byte, 1024)
+
+	for {
+		bytesRead, err := con.Read(buf)
+		if err != nil {
+			if err.Error() != "EOF" {
+				fmt.Println("Error reading from connection:", err)
+			} else if err.Error() == "EOF" {
+				fmt.Println("Connection Closed")
+			}
+			return
+		}
+
+		message := string(buf[:bytesRead])
+		fmt.Println("Message received:", message)
+		if message == "PING\r\n" {
+			con.Write([]byte("+PONG\r\n"))
+		}
+	}
+}
+
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
@@ -14,11 +38,12 @@ func main() {
 	}
 	defer l.Close()
 
-	con, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		con, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(con)
 	}
-	con.Write([]byte("+PONG\r\n"))
-	defer con.Close()
 }
--- tests/tcp.go
+++ tests/tcp.go
@@ -4,15 +4,35 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"time"
 )
 
 func main() {
 	serverAddr := "0.0.0.0:6379"
-	con, err := net.Dial("tcp", serverAddr)
+	conn, err := net.Dial("tcp", serverAddr)
 	if err != nil {
 		fmt.Println("Error connecting:", err)
 		os.Exit(1)
 	}
-	defer con.Close()
+	defer conn.Close()
 	fmt.Println("Connected to", serverAddr)
+
+	for i := 0; i < 5; i++ {
+		msg := "PING\r\n"
+		_, err := conn.Write([]byte(msg))
+		if err != nil {
+			fmt.Println("Error sending message:", err)
+			return
+		}
+
+		buf := make([]byte, 1024)
+		_, err = conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading response:", err)
+			return
+		}
+
+		fmt.Println("Received:", string(buf))
+		time.Sleep(1 * time.Second)
+	}
 }
